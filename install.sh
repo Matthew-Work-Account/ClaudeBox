168ce75146f792ddb40a5b75b8543f6327225119
Matthew Merlene <matthew.merlene.work@gmail.com>
2026-03-25 11:21:09 -0500

    Fix install.sh to copy modules/ directory to CLAUDEBOX_HOME

--- install.sh
+++ install.sh
@@ -91,6 +91,7 @@ chmod +x "${CLAUDEBOX_HOME}/uninstall.sh"
 cp -r "${SCRIPT_DIR}/lib" "${CLAUDEBOX_HOME}/"
 cp -r "${SCRIPT_DIR}/languages" "${CLAUDEBOX_HOME}/"
 cp -r "${SCRIPT_DIR}/.devcontainer" "${CLAUDEBOX_HOME}/"
+[[ -d "${SCRIPT_DIR}/modules" ]] && cp -r "${SCRIPT_DIR}/modules" "${CLAUDEBOX_HOME}/"
 
 # --- Save repo URL for upgrades ---
 repo_url=$(git -C "$SCRIPT_DIR" remote get-url origin 2>/dev/null || true)
